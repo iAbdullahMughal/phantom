0cea5ff707791302cbbb8bf735b854d1652ee70a
Muhammad Abdullah <iamabdullahmughal@gmail.com>
2020-08-12 03:23:02 +0500

    Query issue

--- app/core/tasks/task.py
+++ app/core/tasks/task.py
@@ -11,15 +11,13 @@ def fine_social_user(user_name):
     from app.core.modules.sherlock.sherlock import search_username
 
     try:
-        content = SocialUserSearch.objects.get(username=user_name,
-
-                                               )
+        content = SocialUserSearch.objects.get(username=user_name)
         SharedContent.USER_REF = content
         try:
             search_username(user_name)
         except:
             pass
-        SocialUserSearch.objects.filter(pk=content.id).update(finished_at=now(), search_status=True)
+        SocialUserSearch.objects.filter(id=content.id).update(finished_at=now(), search_status=True)
 
     except Exception as e:
-        print('Failed at start execution' + str(e))
+        print('Failed at start execution ' + str(e))
