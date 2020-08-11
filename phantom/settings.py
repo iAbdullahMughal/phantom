7c52e85931a57e12178bdd7fd86a35a13f8aeec0
Muhammad Abdullah <iamabdullahmughal@gmail.com>
2020-08-12 02:07:19 +0500

    Celery worker configuration
    Heroku celery worker configuration is not working.

--- phantom/settings.py
+++ phantom/settings.py
@@ -114,12 +114,8 @@ STATICFILES_DIRS = (
 )
 
 # celery configuration
-# CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_BROKER_URL = os.environ['REDIS_URL']
-
-# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
-
+BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
