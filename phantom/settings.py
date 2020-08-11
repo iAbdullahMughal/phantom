d32bcfa4ae7055dd0254ff3ad76bc87cc7eed9cd
Muhammad Abdullah <iamabdullahmughal@gmail.com>
2020-08-12 01:57:50 +0500

    Celery backend information
    Heroku supported url

--- phantom/settings.py
+++ phantom/settings.py
@@ -114,8 +114,12 @@ STATICFILES_DIRS = (
 )
 
 # celery configuration
-CELERY_BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_BROKER_URL = os.environ['REDIS_URL']
+
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = os.environ['REDIS_URL']
+
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
